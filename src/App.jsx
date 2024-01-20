6b2fd26eb9de09983ae71b13df2dd76ea305b64b
Shantanu <shantanu.ingale22@gmail.com>
2024-01-20 18:08:44 +0530

    half done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+import NewsAll from "./components/NewsAll"
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <NewsAll/>
+    </div>
+  )
+}
+
+export default App
